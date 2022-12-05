a25b4d7a1cf2abd2ce592a348614369dee30427a
Sergey Zadorozhniy <wiseacreserj@gmail.com>
2022-12-05 05:12:51 +0200

    sidebar basic html and css with responsive done

---
+++ sidebar/js/index.js
@@ -0,0 +1,9 @@
+const mobileLinksContainer = document.querySelector(".mobile-links-container");
+const menuBtn = document.querySelector(".menu-icon");
+
+/* window.addEventListener("resize", () => {
+    if (window.innerWidth >= 850) {
+        mobileLinksContainer.style.display = "none";
+    }
+});
+ */
