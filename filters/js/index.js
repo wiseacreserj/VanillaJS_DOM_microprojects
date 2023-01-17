01146f3a6a3d19fc256cd30f89fb9eff6674ad83
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2023-01-17 02:05:38 +0200

    filters microproject: done

--- filters/js/index.js
+++ filters/js/index.js
@@ -1,37 +1,84 @@
+import { products } from "./data.js";
+
 //Elements
+const productsList = document.querySelector(".l-products");
+const input = document.querySelector(".c-filters__input");
+const filterBtns = document.querySelectorAll(".c-filters__item");
 
-const body = document.querySelector("body");
-const toggleBtn = document.querySelector(".c-toggle-theme-btn");
+//Functions
 
-//functions
+const renderProducts = (container, products) => {
+    container.innerHTML = "";
+    products.forEach((product) => {
+        let template = `<div class="c-product">
+            <div class="c-product__img-container">
+                <img
+                src=${product.image}
+                alt=${product.title}
+                class="c-product__img"
+                />
+            </div>
+            <h4 class="c-product__name">${product.title}</h4>
+            <p class="c-product__price">${product.price}</p>
+        </div>`;
+        container.innerHTML += template;
+    });
+};
+
+const renderAllProducts = () => {
+    renderProducts(productsList, products);
+};
+
+const renderEmptySearchResult = () => {
+    productsList.innerHTML = `<p class="c-error">Sorry, no products matched your search</p>`;
+};
+
+const filterProductsByCompany = (company = "all") => {
+    if (company === "all") {
+        renderProducts(productsList, products);
+        return;
+    }
+    const filteredList = products.filter(
+        (product) => product.company === company
+    );
+
+    renderProducts(productsList, filteredList);
+};
+
+const filterProductsByName = (name) => {
+    if (!name) {
+        renderAllProducts();
+        return;
+    }
+
+    const searchString = name.toLowerCase();
+    const filteredList = products.filter((product) =>
+        product.title.includes(searchString)
+    );
+
+    if (filteredList.length === 0) {
+        renderEmptySearchResult();
+        return;
+    }
+    renderProducts(productsList, filteredList);
+};
 
 const initApp = () => {
-    let themeStatus = JSON.parse(localStorage.getItem("themeStatus"));
-
-    if (themeStatus.isDark) {
-        body.classList.add("dark-theme");
-    }
+    renderAllProducts();
 };
 
-const saveThemeToLocalStorage = (boolean) => {
-    let themeStatus = {
-        isDark: boolean,
-    };
+//Assign listeners
+input.addEventListener("input", (event) => {
+    event.preventDefault();
+    filterProductsByName(event.target.value);
+});
 
-    localStorage.setItem("themeStatus", JSON.stringify(themeStatus));
-};
+filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        const company = btn.getAttribute("data-company");
+        input.value = "";
+        filterProductsByCompany(company);
+    });
+});
 
-//Listeners
-const toggleTheme = () => {
-    if (body.classList.contains("dark-theme")) {
-        body.classList.remove("dark-theme");
-        saveThemeToLocalStorage(false);
-    } else {
-        body.classList.add("dark-theme");
-        saveThemeToLocalStorage(true);
-    }
-};
-
-toggleBtn.addEventListener("click", toggleTheme);
-
-window.addEventListener("DOMContentLoaded", initApp);
+initApp();
