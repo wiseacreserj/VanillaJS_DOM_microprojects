a5570403fc28a8927bf2258ab58fab68acb7b038
Sergey Zadorozhniy <wiseacreserj@gmail.com>
2023-01-21 03:32:56 +0200

    products microproject: js done, 80% final version layout done

--- products/js/index.js
+++ products/js/index.js
@@ -1,8 +1,45 @@
 //API
 
-import { fetchAllProducts, fetchProductById } from "./api.js";
+import { fetchAllProducts } from "./api.js";
 
+//Elements
 const productsContainer = document.querySelector(".l-products");
+const spiner = document.querySelector(".c-spinner");
 
-fetchAllProducts();
-fetchProductById("rec4f2RIftFCb7aHh");
+//Functions
+
+const renderProducts = async () => {
+    const products = await fetchAllProducts();
+    spiner.remove();
+
+    const template = products
+        .map((product) => {
+            const id = product.id;
+            const {
+                price,
+                name,
+                image: [{ url: imageURL }],
+            } = product.fields;
+            return `<a href="product.html?id=${id}">
+                <div class="c-product-card">
+                    <div class="c-product-card__img-container">
+                        <img
+                            src=${imageURL}
+                            alt=${name}
+                            class="c-product-card__img"
+                        />
+                    </div>
+                    <h4 class="c-product-card__name">${name}</h4>
+                    <p class="c-product-card__price">$${price / 100}</p>
+                </div></a
+            >`;
+        })
+        .join("");
+    productsContainer.innerHTML = template;
+};
+
+const initApp = () => {
+    renderProducts();
+};
+
+initApp();
